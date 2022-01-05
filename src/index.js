88f18b3aea3d12252bd695c8d513add67d5af8f8
Wadzim <w4dzim@gmail.com>
2022-01-05 19:12:49 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  (n < 0) ? n = -n : n;
+  let str = String(n);
+  let arr = str.split('');
+  let arrRev = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    arrRev.push(arr[i]);
+  }
+  return +arrRev.join('');
 }
+
